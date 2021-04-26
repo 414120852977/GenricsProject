ad447567be7de78a1ca928746c7a308926ba007e
Ashok Mane <ashokmane81800@gmail.com>
2021-04-26 12:32:02 +0530

    [ashok] created a java project to gentrate welcome message using genrics

---
+++ FindMaximum.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class FindMaximum {
+    public static void main(String[] args) {
+        System.out.println("**welcome to Find Maximum value Using Genrics**");
+    }
+}
